4e28f9ce59e858290d0ac46f1f75ddc607e48b65
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-07-07 11:18:48 -0400

    all deliverables completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 //ramen url
 const url = 'http://localhost:3000/ramens';
+//global ramen id variable for editing
+let currRamenId = 0;
 //grab ramen menu
 const ramenMenu = document.getElementById('ramen-menu');
 //grab the selected ramen image
@@ -16,6 +18,8 @@ const selectedComment = document.querySelector('#comment-display');
 const newRamen = document.querySelector('#new-ramen');
 //grab the form to edit current ramen
 const editRamen = document.querySelector('#edit-ramen');
+//grab the form element to delete current ramne
+const deleteRamen = document.querySelector("#delete")
 
 
 const fetchRamen = async () => {
@@ -27,6 +31,8 @@ const fetchRamen = async () => {
 };
 
 const renderMenu = async () => {
+    //clear the div
+    ramenMenu.innerHTML = '';
     //get data struct from promise
     let currMenu = await fetchRamen();
     //iterate through each, make new image element and append
@@ -53,6 +59,9 @@ const renderMenu = async () => {
             selectedRating.textContent = ramenItem.rating;
             //set ramen comment
             selectedComment.textContent = ramenItem.comment;
+            //change global variable (USE THIS TO EDIT)
+            currRamenId = ramenItem.id;
+            console.log(currRamenId)
         })
     });
 }
@@ -69,10 +78,17 @@ const setInitial = async () => {
     selectedRestaurant.textContent = currMenu[0].restaurant;
     selectedRating.textContent = currMenu[0].rating;
     selectedComment.textContent = currMenu[0].comment;
+
+    //set curr id
+    currRamenId = currMenu[0].id;
 }
+
+//adding new ramen entry event listener 
 newRamen.addEventListener('submit', (event) => {
+    //prevent refresh
     event.preventDefault();
 
+    //make new object to hold info
     const newRamenEntry = {
         name: newRamen['name'].value,
         restaurant: newRamen['restaurant'].value,
@@ -81,15 +97,17 @@ newRamen.addEventListener('submit', (event) => {
         comment: newRamen['new-comment'].value,
     }
 
+    //stringified obj 
     let newRamenEntryDb = JSON.stringify(newRamenEntry);
 
-
+    //post that bitch into the db
     let addNew = fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json",
         },
+        //attach here
         body: newRamenEntryDb,
     })
 
@@ -118,9 +136,67 @@ newRamen.addEventListener('submit', (event) => {
     })
 })
 
-editRamen.addEventListener('submit', (event) => {
+editRamen.addEventListener('submit', async (event) => {
+    alert('pressing submit')
+    //prevent default
     event.preventDefault();
+    //fetch array
+    let currMenu = await fetchRamen();
+    console.log(editRamen['rating'].value);
+    console.log(editRamen['new-comment'].value);
 
+    //if rating is not blank, update JUST the RATING
+    if(editRamen['rating'].value != ''){
+        fetch(`${url}/${currRamenId}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                rating: editRamen['rating'].value,
+            }),
+            headers: {'Content-type': 'application/json; charset=UTF-8'}
+        }).then((response) => response.json());
+        //correct current display
+        selectedRating.textContent = editRamen['rating'].value;
+    }
+
+    //if new comment is not blank, update JUST the COMMENT
+    if(editRamen['new-comment'].value != ''){
+        fetch(`${url}/${currRamenId}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                comment: editRamen['new-comment'].value,
+            }),
+            headers: {'Content-type': 'application/json; charset=UTF-8'}
+        }).then((response) => response.json());
+        //correct current display
+        selectedComment.textContent = editRamen['new-comment'].value;
+    }
+})
+
+//listen for delete button being pressed
+deleteRamen.addEventListener('click', async (event) => {
+    let currMenu = await fetchRamen();
+
+    //make sure there is something to delete first
+    if(currMenu.length > 0){
+        //ask user for confirmation
+        if(confirm('Are you sure you want to delete this entry?')){
+            //delete request
+            fetch(`${url}/${currRamenId}`, {
+                method: 'DELETE',
+            }).then((response) => {
+                //you have to rerender the menu within the then because of asynchronus op
+                //if done out side of the fetch, then it will execute before the promise resolves
+                response.json()
+                renderMenu();
+                setInitial();
+            });
+
+        } else {
+            alert('Deletion cancelled');
+        }
+    } else {
+        alert('ERROR: Nothing left to be deleted')
+    }
     
 })
 
